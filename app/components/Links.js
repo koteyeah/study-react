d33d7273cd93ed0dc883036e33cf4d0631967701
koteyeah <k.kouta2346@gmail.com>
2024-01-14 23:56:04 +0900

    🎨 map関数でコードを簡略化。map関数は配列の各要素に対して、コールバック関数を実行して返り値を配列で返す

--- app/components/Links.js
+++ app/components/Links.js
@@ -1,57 +1,41 @@
 import styles from '../page.module.css'
 
+const ITEMS =[
+    { href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+    title: "Docs",
+    description: "Find in-depth information about Next.js features and API."
+    },
+    { href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+    title: "Learn",
+    description: "Learn about Next.js in an interactive course with&nbsp;quizzes!"
+    },
+    { href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+    title: "Templates",
+    description: "Explore starter templates for Next.js."
+    },
+    { href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+    title: "Deploy",
+    description: "Instantly deploy your Next.js site to a shareable URL with Vercel."
+    }
+]
+
 export function Links() {
   return (
     <div className={styles.grid}>
-    <a
-        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        className={styles.card}
+        {ITEMS.map(item=>{
+            return (
+                <a
+        href={item.href} className={styles.card}
         target="_blank"
         rel="noopener noreferrer"
     >
         <h2>
-        Docs <span>-&gt;</span>
+        {item.title} <span>-&gt;</span>
         </h2>
-        <p>Find in-depth information about Next.js features and API.</p>
+        <p>{item.description}</p>
     </a>
-
-    <a
-        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-    >
-        <h2>
-        this is index <span>-&gt;</span>
-        </h2>
-        <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-    </a>
-
-    <a
-        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-    >
-        <h2>
-        Templates <span>-&gt;</span>
-        </h2>
-        <p>Explore starter templates for Next.js.</p>
-    </a>
-
-    <a
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-    >
-        <h2>
-        Deploy <span>-&gt;</span>
-        </h2>
-        <p>
-        Instantly deploy your Next.js site to a shareable URL with Vercel.
-        </p>
-    </a>
+            )
+        })}
     </div>
   )
 }
