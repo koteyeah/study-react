b2dbc042433eb064090e0f43c4b467a4da4625e8
koteyeah <k.kouta2346@gmail.com>
2024-01-15 00:07:05 +0900

    🎨

--- app/components/Links.js
+++ app/components/Links.js
@@ -7,7 +7,7 @@ const ITEMS =[
     },
     { href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
     title: "Learn",
-    description: "Learn about Next.js in an interactive course with&nbsp;quizzes!"
+    description: "Learn about Next.js in an interactive course with quizzes!"
     },
     { href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
     title: "Templates",
