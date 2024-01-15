fed9ec4e403449f4501ee5d5d127ffddce1d1e03
koteyeah <k.kouta2346@gmail.com>
2024-01-15 23:37:01 +0900

    名称の変更

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,16 @@ import styles from '/app/page.module.css'
 import { Headline } from '/app/components/Headline'
 import Link from 'next/link'
 import { Links } from '@/app/components/Links'
+import { useEffect } from 'react'
 export default function Home() {
+  //このDOMがレンダリングされる瞬間に実行される関数
+  useEffect(()=>{
+    document.body.style.backgroundColor="red";
+  //アンマウント時に実行される
+    return()=>{
+    document.body.style.backgroundColor="";
+    }
+    },[])
   return (
     <main className={styles.main}>
         <Headline title="page.jsx">
